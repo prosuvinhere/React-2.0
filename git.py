6c4f49a4ab4559778b931c3cf1f5ed48e3c1ba7f
prosuvinhere <suvinsingh2003@gmail.com>
2024-06-30 14:47:55 +0530

    adding new featues

--- git.py
+++ git.py
@@ -1,13 +1,13 @@
 import os , random
 
-for i in range(10):
+for i in range(50):
     d = str(i) + 'days ago'
     rand = random.randrange(0, 12)
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2023-'+str(rand)+'-'+d+'" -m 1')
-os.system('git push -u origin main')
+    os.system('git commit --date=" 2024-'+str(rand)+'-'+d+'" -m 1')
+os.system('git push -u web master')
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
 #git fetch origin master
